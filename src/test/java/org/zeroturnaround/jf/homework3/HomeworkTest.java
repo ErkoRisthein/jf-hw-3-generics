1bb14f55ee87c69104677f74aa42f91de2e7abf7
Arnel Pällo <arnel.pallo@zeroturnaround.com>
2017-02-13 02:25:22 +0200

    oops

--- src/test/java/org/zeroturnaround/jf/homework3/HomeworkTest.java
+++ src/test/java/org/zeroturnaround/jf/homework3/HomeworkTest.java
@@ -1,13 +1,5 @@
 package org.zeroturnaround.jf.homework3;
 
-import org.junit.Test;
-import java.io.IOException;
-
 public class HomeworkTest {
-
-  @Test
-  public void testREADMEmd() throws IOException {
-
-  }
 
 }
