ff424bc8623f35654ce0f2e35a0d0662d97007f9
Arnel Pällo <arnel.pallo@zeroturnaround.com>
2017-02-13 03:58:06 +0200

    rename

--- src/main/java/org/zeroturnaround/jf/homework3/task2/Task2.java
+++ src/main/java/org/zeroturnaround/jf/homework3/task2/Task2.java
@@ -2,7 +2,7 @@ package org.zeroturnaround.jf.homework3.task2;
 
 import java.util.List;
 
-public class Task1 {
+public class Task2 {
 
   public static List unique(List... lists) {
     throw new RuntimeException("Add generics to signature and implement me");
