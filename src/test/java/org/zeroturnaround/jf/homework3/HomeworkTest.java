9bdf62b9c9b2ca8063b99d7abd10e6ede41dc5e9
Arnel Pällo <arnel.pallo@zeroturnaround.com>
2017-02-13 02:06:23 +0200

    first commit

---
+++ src/test/java/org/zeroturnaround/jf/homework3/HomeworkTest.java
@@ -0,0 +1,13 @@
+package org.zeroturnaround.jf.homework3;
+
+import org.junit.Test;
+import java.io.IOException;
+
+public class HomeworkTest {
+
+  @Test
+  public void testREADMEmd() throws IOException {
+
+  }
+
+}
